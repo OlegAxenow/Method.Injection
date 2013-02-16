0f94de5e73e00c6195c524122666206ac4249bdb
Oleg Aksenov <OlegAxenow@server.fake>
2013-02-17 00:39:23 +0400

    Fix adding to list

--- Method.Inject.Spec/Injections/DoWorkInjectionDirect.cs
+++ Method.Inject.Spec/Injections/DoWorkInjectionDirect.cs
@@ -2,11 +2,14 @@ using Method.Inject.Spec.Types;
 
 namespace Method.Inject.Spec.Injections
 {
+	/// <summary>
+	/// Test direct type injection (without interfaces).
+	/// </summary>
 	public class DoWorkInjectionDirect : MethodInjection
 	{
 		public virtual void DoWork(BaseType instance, string parameter)
 		{
-			instance.CallsLog.Add(GetType().Name + ".DoWork(" + parameter + ")");
+			instance.CallsLog.Add(GetType().Name + ".DoWorkInjectionDirect(" + parameter + ")");
 		}
 	}
 }
